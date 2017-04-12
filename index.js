42f4e1f58164b5f4895c08f748f4c383667b7610
Lev Himmelfarb <lev@boylesoftware.com>
2017-04-12 18:05:07 -0400

    Work in progress.

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 /**
  * Central module for building RESTful web services.
  *
- * @module x2node-webservice
+ * @module x2node-webservices
  */
 'use strict';
+
+const Application = require('./lib/application.js');
+
+
+/**
+ * Application configuration options.
+ *
+ * @typedef {Object} ApplicationOptions
+ * @property {number} initialRequestTimeout Timeout in milliseconds for the
+ * client to issue the HTTP request complete with headers after opening
+ * connection to the web service. The default is 30 seconds.
+ * @property {number} maxRequestHeadersCount Maximum allowed number of incoming
+ * HTTP request headers. The default is 50.
+ */
+
+/**
+ * Create application the represents the web service. The application must be
+ * configured before it's run and starts responding to the incoming requests.
+ *
+ * @param {module:x2node-webservices~ApplicationOptions} [options] Application
+ * configuration options.
+ * @returns {module:x2node-webservices~Application} The application.
+ */
+exports.createApplication = function(options) {
+
+	return new Application(options || {});
+};
